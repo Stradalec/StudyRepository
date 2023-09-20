4712f5768970a8daf8eb4e24ebb5858a73390ec2
Stradalec <pochtatipo6@gmail.com>
2023-09-20 16:50:33 +0700

    Finalfix FirstTask.cpp
    Fixed some style problems

--- FirstTask.cpp
+++ FirstTask.cpp
@@ -2,7 +2,7 @@
 * Автор: Калинин А.А.             *
 * Дата: 15.09.2023                *
 * Название: Вариант номер 3       *
-* https://onlinegdb.com/1Ngggqaot *
+* https://onlinegdb.com/amHjaNO5f *
 **********************************/
 
 #include <iostream>
@@ -11,27 +11,32 @@
 using namespace std;
 
 int main() {
-  double angle = 0.2443460952; /* The value is changed due to the fact
+  int degree;
+  double radian; /* The value is changed due to the fact
   that the sin(n) formula works for radians */
   int initialSpeed = 750;
-  double gravity = 9.81;
+  double gravity   = 9.81;
   double flightTime;
   double maxFlightHeight;
   double maxFlightRange;
   double trajectoryRadiusBegin;
   double trajectoryRadiusVertex;
 
-  flightTime = (2 * initialSpeed * (sin(angle))) / gravity;
-  maxFlightHeight = ((pow(initialSpeed, 2) * pow(sin(angle), 2)) / (2 * gravity));
-  maxFlightRange = ((pow(initialSpeed, 2) * sin(2 * angle)) / gravity);
-  trajectoryRadiusBegin = pow(initialSpeed, 2) / (gravity * cos(angle));
-  trajectoryRadiusVertex = ((pow(initialSpeed, 2) * pow(cos(angle), 2)) / gravity);
+  cout << "Angle = ";
+  cin >> degree;
+
+  radian                 = degree * (M_PI / 180);
+  flightTime             = (2 * initialSpeed * ( sin(radian) ) ) / gravity; 
+  maxFlightHeight        = ( (pow(initialSpeed, 2) * pow( sin(radian), 2) ) / (2 * gravity) );
+  maxFlightRange         = ( (pow(initialSpeed, 2) * sin(2 * radian)) / gravity);
+  trajectoryRadiusBegin  = pow(initialSpeed, 2) / (gravity * cos(radian) );
+  trajectoryRadiusVertex = ( (pow(initialSpeed, 2) * pow( cos(radian), 2) ) / gravity);
 
   cout.precision(11);
-  cout << "Flight time: " << flightTime << '\n'
-	   << "Max flight height: " << maxFlightHeight << '\n'
-	   << "Max flight range: " << maxFlightRange << '\n'
-       << "Radius of curvature of the beginning of the trajectory: " << trajectoryRadiusBegin << '\n'
-       << "Radius of curvature of the vertex of the trajectory: " << trajectoryRadiusVertex << endl;
+  cout << "Flight time: "       << flightTime      << '\n'
+       << "Max flight height: " << maxFlightHeight << '\n'
+       << "Max flight range: "  << maxFlightRange  << '\n'
+       << "Radius of curvature of a beginning of the trajectory: " << trajectoryRadiusBegin  << '\n'
+       << "Radius of curvature of a vertex of the trajectory: "    << trajectoryRadiusVertex;
   return 0;
 }
